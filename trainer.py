db998c77e4d3e3265ae753e289b518fd333e8992
Luke Martin <lukechile@gmail.com>
2020-05-07 16:19:02 -0400

    Update trainer.py

--- trainer.py
+++ trainer.py
@@ -130,7 +130,7 @@ class Trainer:
 
         train_dataset = self.dataset(
             self.opt.data_path, train_filenames, self.opt.height, self.opt.width,
-            self.opt.frame_ids, self.opt.num_scales, is_train=True, img_ext=img_ext)
+            self.opt.frame_ids,  self.opt.num_scales, is_train=True, img_ext=img_ext)
         self.train_loader = DataLoader(
             train_dataset, self.opt.batch_size, True,
             num_workers=self.opt.num_workers, pin_memory=True, drop_last=True)
@@ -138,7 +138,7 @@ class Trainer:
 
         val_dataset = self.dataset(
             self.opt.data_path, "val", self.opt.height, self.opt.width,
-            self.opt.frame_ids, self.opt.num_scales, is_train=False, img_ext=img_ext)
+            self.opt.frame_ids,  self.opt.num_scales, is_train=False, img_ext=img_ext)
 
         self.val_loader = DataLoader(
             val_dataset, self.opt.batch_size, True,
